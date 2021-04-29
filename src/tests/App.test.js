30a953fddbf185c7a73c796dd55262b29b177121
wiegand92 <wiegand.alex@gmail.com>
2021-04-29 11:50:17 -0400

    set up testing env

---
+++ src/tests/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import App from '../components/App';
+
+test('renders README link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/here/i);
+  expect(linkElement).toBeInTheDocument();
+});
