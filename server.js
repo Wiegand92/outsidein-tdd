e48aeeaaf1849a37e37db02913b0d868ee622cf7
wiegand92 <wiegand.alex@gmail.com>
2021-04-29 14:22:27 -0400

    specify page should list restaurants, configure cypress to process env vars

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 
 // If we deploy, will set the port to whatever server, else 4200 //
 const PORT = process.env.PORT || 4200;
@@ -14,8 +15,9 @@ app.use(express.json());
 
 app.use(express.static(__dirname + '/public'));
 
+console.log(process.env.API_KEY);
 
 // Start Server //
 app.listen(PORT, () => {
-  console.log(`server started on port: ${PORT}`)
+  console.log(`server started on port: ${PORT}`);
 });
